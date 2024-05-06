1bd94c27eb63f84b8509fbae07614f4a7aec71b6
isnendyankp <isnendyankp@gmail.com>
2024-05-06 21:49:15 +0700

    Update database URI in main.py and utils/db.py

--- main.py
+++ main.py
@@ -16,8 +16,8 @@ CORS(app)
 load_dotenv()
 
 # Setting database URI directly
-# app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:RwpRYvCgwTIqjgoUQDPBMnCVpPLxudri@roundhouse.proxy.rlwy.net:47006/railway"
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
+# app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:RwpRYvCgwTIqjgoUQDPBMnCVpPLxudri@roundhouse.proxy.rlwy.net:47006/railway"
 
 # Initializing database
 init_db(app)
